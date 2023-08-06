ab3cf1c70f62ce74d978b200482b3096c17aafd0
K1r1llLukoyanov <kirill.lukoyanow@gmail.com>
2023-08-06 22:24:21 +0300

    Fixed

--- GFG/Backtracking/equal_sum_partition.cpp
+++ GFG/Backtracking/equal_sum_partition.cpp
@@ -14,41 +14,53 @@ void display(vector<vector<int>> &solution){
 }
 
 
-void partition(vector<int>& nums, vector<vector<int>>& solution, vector<int>& sums, int left, int pos){
-	int n = nums.size();
-	int m = solution.size();
-	
-	if(pos == n){
-		int first = sums[0];
-		for(int i = 1; i < m; i++){
-			if(first != sums[i])
-				return;
-		}
-		display(solution);
-		return;
+bool prove(vector<int>& nums, vector<int> used, vector<int> subsum, int sum, int curr, int n){
+	int k = subsum.size();
+	if(subsum[curr] == sum){
+		if(curr == k-2)
+			return true;
+		return prove(nums, used, subsum, sum, curr+1, n-1);
 	}
-
-	for(int i = pos; i < n; i++){
-		for(int j = 0; j < m; j++){
-			solution[j].push_back(nums[i]);
-			sums[j] += nums[i];
-			partition(nums, solution, sums, left-nums[i], i+1);
-			solution[j].pop_back();
-			sums[j] -= nums[i];
+	for(int i = n; i >= 0; i--){
+		if(used[i])
+			continue;
+		int temp = subsum[curr] + nums[i];
+		if(temp <= sum){
+			used[i] = 1;
+			subsum[curr] = temp;
+			if(prove(nums, used, subsum, sum, curr, i-1))
+				return true;
+			used[i] = 0;
+			subsum[curr] -= nums[i];
 		}
 	}
+	return false;
 }
 
+
+bool is_possible(vector<int>& nums, int k){
+	int n = nums.size();
+	int sum = 0;
+	for(int i = 0; i < n; i++)
+		sum += nums[i];
+	if(sum % k)
+		return false;
+	int subsetSum = sum / k;
+	vector<int> subset;
+	for(int i = 0; i < k; i++)
+		subset.push_back(0);
+	vector<int> used;
+	for(int i = 0; i < n; i++)
+		used.push_back(0);
+	return prove(nums, used, subset, subsetSum, 0, n-1);
+}
 
 int main(){
 	vector<int> nums = {2, 1, 4, 5, 6};
 	int k = 3;
-	vector<vector<int>> solution;
-	vector<int> sums;
-	for(int i = 0; i < k; i++){
-		solution.push_back({});
-		sums.push_back(0);
-	}
-	partition(nums, solution, sums, 18, 0);
+	if(is_possible(nums, k))
+		cout << "It's possible\n";
+	else
+		cout << "It's not possible\n";
 	return 0;
 }
